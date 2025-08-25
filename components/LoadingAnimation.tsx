80d6950bddbb924e23723a6a94b5e2985eff2ad3
Joseph Hickman <joe@brandureai.com>
2025-08-25 13:04:38 +0400

    Switch loading animation logo to white version: change from brandure-logo-new-colour.png to brandure-logo-new-white.png for better visibility on dark background

--- components/LoadingAnimation.tsx
+++ components/LoadingAnimation.tsx
@@ -488,7 +488,7 @@ export default function LoadingAnimation({ onComplete }: { onComplete: () => voi
         title="Enter Brandure AI"
       >
         <img
-          src="/Logos/brandure-logo-new-colour.png"
+          src="/Logos/brandure-logo-new-white.png"
           alt="Brandure AI"
           style={{
             maxWidth: '200px',
