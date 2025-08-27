8828619b203559721073caab1440a0db1b88c2aa
Joseph Hickman <joe@brandureai.com>
2025-08-27 15:49:09 +0400

    Add Vercel Speed Insights: install @vercel/speed-insights and render <SpeedInsights /> in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import FloatingGradientCanvas from '../components/FloatingGradientCanvas';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <FloatingGradientCanvas />
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
